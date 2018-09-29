c7c4ce3972df385261c59caac71ebef203380ae6
李灝 <lihao@lihaode-MacBook-Pro.local>
2018-09-29 09:05:20 +0800

    subs

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 8080;
 
 //把所有人儲存在users
 var users = {};
 var user_count = 0;
-app.use('/',express.static(__dirname + '/dist'));
-app.use('/favicon.png', express.static('src/favicon.png'));
+app.use("/", express.static(__dirname + "/dist"));
+app.use("/favicon.png", express.static("src/favicon.png"));
 
 // app.get('/', function (req, res) {
 //     res.sendFile('./dist/index.html', { root: __dirname });
 // });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + './dist/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "./dist/index.html");
 });
 
 //sockeio area
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on("connection", function(socket) {
+  console.log("a user connected");
   var user = {};
   //使用者按下登陸之後 產生id time 等等細節
   // {
@@ -28,9 +28,9 @@ io.on('connection', function(socket){
   //   time:'',
   //   socke:(socket)
   // }
-  socket.on('login', function(callback){
+  socket.on("login", function(callback) {
     user.id = Math.floor(Math.random() * (9999 - 1 + 1) + 1);
-    user.time = (new Date()).getTime();
+    user.time = new Date().getTime();
     //把這個socket也存進去
     user.socket = socket;
     user.to = false;
@@ -41,64 +41,59 @@ io.on('connection', function(socket){
     for (var _id in users) {
       console.log(_id);
       if (!users[_id].to && _id != user.id) {
-          //從最前面的找起
-          users[_id].to = user.id;
+        //從最前面的找起
+        users[_id].to = user.id;
 
-          //並且傳 start給遠端 並帶入 對方id  這邊很重要
-          user.socket.emit("start", _id, user.id);
-          //user聊天對象也定義玩
-          user.to = _id;
+        //並且傳 start給遠端 並帶入 對方id  這邊很重要
+        user.socket.emit("start", _id, user.id);
+        //user聊天對象也定義玩
+        user.to = _id;
 
-          //對方等待者也要接受start 對象
-          users[_id].socket.emit("start", user.id, _id);
+        //對方等待者也要接受start 對象
+        users[_id].socket.emit("start", user.id, _id);
       }
     }
-    console.log(user.id + " to "+user.to);
+    console.log(user.id + " to " + user.to);
   });
 
-
-    socket.on('disconnect', function(){
-      if (user.to && users[user.to] && users[user.to].socket) {
-          //傳給對象 跟他說我離開了
-          users[user.to].socket.emit('userexit');
-      }
-      //刪除離開的那個人
-      delete(users[user.id]);
-      delete(users[user.to]);
-    });
-
-
-  socket.on('_userexit', function(){
-    exchange('userexit');
-    users[user.id].socket.emit('userexit');
-    delete(users[user.id]);
-    delete(users[user.to]);
+  socket.on("disconnect", function() {
+    if (user.to && users[user.to] && users[user.to].socket) {
+      //傳給對象 跟他說我離開了s
+      users[user.to].socket.emit("userexit");
+    }
+    //刪除離開的那個人
+    delete users[user.id];
+    delete users[user.to];
   });
 
-  socket.on('submit', function(msg,whoid){
-    console.log('submit: '+ msg.text +'from '+ whoid);
+  socket.on("_userexit", function() {
+    exchange("userexit");
+    users[user.id].socket.emit("userexit");
+    delete users[user.id];
+    delete users[user.to];
+  });
+
+  socket.on("submit", function(msg, whoid) {
+    console.log("submit: " + msg.text + "from " + whoid);
     msg.whoid = whoid;
-    if(msg.text){
-      if(msg.text!=" "){
-    exchange("messageAdd",msg)
+    if (msg.text) {
+      if (msg.text != " ") {
+        exchange("messageAdd", msg);
       }
     }
     //io.emit('messageAdd', msg);
   });
 
   function exchange(evt, data) {
-      if (user.to && users[user.to] && users[user.to].socket) {
-          users[user.to].socket.emit(evt, data);
-          users[user.id].socket.emit(evt, data);
-      }
+    if (user.to && users[user.to] && users[user.to].socket) {
+      users[user.to].socket.emit(evt, data);
+      users[user.id].socket.emit(evt, data);
+    }
   }
-
-
 });
-
 
 //--socketio area
 
-http.listen(port, function(){
-  console.log('listening on *:'+port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
